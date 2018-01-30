67c2dca12265ee33551bc273502275f01c8a591c
Sid Vishnoi <sidvishnoi8@gmail.com>
2018-01-30 18:24:19 +0530

    Move content data to _data/

--- scripts/build/build.js
+++ scripts/build/build.js
@@ -4,6 +4,11 @@ const buildJson = require('./build-json');
 const waterfall = require('async/waterfall');
 const applyEach = require('async/applyEach');
 const yaml = require('node-yaml');
+const path = require('path');
+const { paths } = require('./utils');
+
+paths.eventsData = path.resolve(paths.content, './_data/events.yaml');
+paths.sponsorsData = path.resolve(paths.content, './_data/sponsors.yaml');
 
 const generate = (page, content, cb) =>
   applyEach(
@@ -18,11 +23,11 @@ const generate = (page, content, cb) =>
 
 const build = (page, callback) => {
   const events = page.slug.includes('events/')
-    ? yaml.readSync('../../src/content/events.yaml')
+    ? yaml.readSync(paths.eventsData)
     : {};
 
   const { sponsors } = page.slug.includes('sponsors/')
-    ? yaml.readSync('../../src/content/sponsors.yaml')
+    ? yaml.readSync(paths.sponsorsData)
     : { sponsors: [] };
 
   return waterfall([
