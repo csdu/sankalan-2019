ca97b7f6b71a100d52e90dd0bb1cafb4b705d1bd
Sid Vishnoi <sidvishnoi8@gmail.com>
2018-02-02 00:41:49 +0530

    Add #bada55 message for console Update footer attribution Add prize amount on front page

--- src/js/header.js
+++ src/js/header.js
@@ -5,6 +5,16 @@
  * Copyright 2018
  */
 
+// print that status message in console :D
+const pprint = {
+  ct: `${'\u00a0'.repeat(13)}Hey you, Hackerman!${'\u00a0'.repeat(16)}
+${'\u00a0'.repeat(2)}I see you are interested in the source code.${'\u00a0'.repeat(2)}
+${'\u00a0'.repeat(13)}Let me help you :)${'\u00a0'.repeat(17)}`,
+  st: 'background: #bada55;color:#000;font-weight: bold;',
+};
+console.log(`%c${pprint.ct}`, pprint.st);
+console.log(`%c${'\u00a0'.repeat(1)}https://github.com/sidvishnoi/sankalan-2018${'\u00a0'.repeat(2)}`, 'color: yellow;padding:4px;background:#000');
+
 const isFrontPage = () =>
   window.location.pathname === '/sankalan/';
 
