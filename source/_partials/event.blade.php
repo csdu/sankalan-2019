7e16b240bca7134ee0ad568af6135a39dc9f9729
Ruman Saleem <ruman63@gmail.com>
2019-02-16 16:48:24 +0530

    list all events as thumbnail tiles

--- source/_partials/event.blade.php
+++ source/_partials/event.blade.php
@@ -6,25 +6,13 @@ $classes = collect([
     "event-fl" => $event->isfunlearn    
 ])->filter()->keys()->implode(' ');
 @endphp
-<div class="module {{ $classes }}">
-        <div class="module--half">
-            @if($event->image)
-                <img src="{{ str_replace('/s1600/', '/w256/', $event->image) }}"/>
-            @endif
-        </div>
-        <div class="module--half">
-            <h2>{{ $event->title }}</h2>
-            @if($event->quote)
-                <h3>
-                    &ldquo;{{ $event->quote['text'] }}&rdquo;
-                    @if(array_key_exists('by', $event->quote))
-                        <span>&mdash; {{ $event->quote['by'] }}</span>
-                    @endif
-                </h3> 
-            @endif
-            <p>
-                {!! str_replace('\n', '</p><p>', $event->description) !!}
-            </p> 
-            <a class="button" href="{{ $event->getUrl() }}" class="xhr" data-id="events-{{ str_slug($event->title) }}">Know More</a>
-        </div>
+<div class="event-tile">
+    @if($event->image)
+    <img class="event-bg" src="{{ str_replace('/s1600/', '/w360/', $event->image) }}"/>
+    @endif
+    <div class="event-overlay {{ $classes }}">
+        <h2 class="event-title text-center">{{ $event->title }}</h2>
+        <p>{!! str_replace('\n', '<br>', str_limit($event->description, 300, '...')) !!}</p>
+        <a class="button text-center" href="{{ $event->getUrl() }}" class="xhr" data-id="events-{{ str_slug($event->title) }}">Know More</a>
     </div>
+</div>
--- source/events.blade.php
+++ source/events.blade.php
@@ -1,5 +1,45 @@
 @extends('_layouts.master')
 @section('title', 'Events | ')
+@push('styles')
+    <style>
+        .event-tiles {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            flex-shrink: 0;
+            flex-grow: 0;
+            margin: -1rem;
+        }
+
+        .event-tile {
+            /* background: white; */
+            margin: 1rem;
+            position: relative;
+            min-width: 340px;
+            max-width: 360px;
+        }
+        .event-tile .event-bg {
+            width: 100%;
+        }
+
+        .event-tile .event-overlay {
+            position: absolute;
+            left: 0;
+            right: 0;
+            top: 0;
+            bottom: 0;
+            display: flex;
+            padding: 1rem;
+            flex-direction: column;
+            justify-content: space-around;
+            background: rgba(255, 255, 255, 0.7); 
+            color: #000;
+        }
+        .event-overlay .event-title {
+            font-size: 1.5rem;
+        }
+    </style>
+@endpush
 @section('content')
 <div class="banner">
     <div class="banner-content">
@@ -12,7 +52,7 @@
 </div>
 <div class="content">
     <div>
-        <div class="section" itemscope itemtype="http://schema.org/ItemList">
+        <div class="section event-tiles" itemscope itemtype="http://schema.org/ItemList">
             @foreach($events as $event)
                 @include('_partials.event', compact('event'))
             @endforeach
