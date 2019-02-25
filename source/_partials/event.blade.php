42529efd7ad9a5bb8b1b6844fd061af0e8469234
Ruman Saleem <ruman63@gmail.com>
2019-02-25 13:46:22 +0530

    Clean Event Overview

--- source/_partials/event.blade.php
+++ source/_partials/event.blade.php
@@ -4,8 +4,8 @@
     <img class="event-bg" src="{{ str_replace('/s1600/', '/w360/', $event->image) }}"/>
     @endif
     <div class="event-overlay {{ $event->tag ? 'event-tag' : ''}}" data-tag="{{ $event->tag }}">
-        <h2 class="event-title text-center">{{ $event->title }}</h2>
-        <p>{!! str_replace('\n', '<br>', str_limit($event->description, 300, '...')) !!}</p>
+        {{-- <h2 class="event-title text-center">{{ $event->title }}</h2> --}}
+        {{-- <p class="event-desc">{!! str_replace('\n', '<br>', str_limit($event->description, 200, '...')) !!}</p> --}}
         <a class="button text-center" href="{{ $event->getUrl() }}" class="xhr" data-id="events-{{ str_slug($event->title) }}">Know More</a>
     </div>
 </div>
