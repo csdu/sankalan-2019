b559801f2632bc46b46e66f0fdd3ea3f46e37cf2
Ruman Saleem <ruman63@gmail.com>
2019-09-12 22:47:36 +0530

    deploy to ducs.in/sanakalan-2019 - resolves #1

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 let mix = require('laravel-mix');
 require('laravel-mix-purgecss');
 let build = require('./tasks/build.js');
-let baseUrl = mix.inProduction() ? '/sankalan/' : '/';
+let baseUrl = mix.inProduction() ? '/sankalan-2019/' : '/';
 
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build');
